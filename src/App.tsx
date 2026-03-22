a08581711653ebcb6a765de1a2f7c06810eb7f35
Chottiwatt Jittprasong <mail@chottiwatt.in.th>
2026-03-22 19:39:09 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,22 +12,17 @@ export default function Page() {
 
   const getNoButtonText = () => {
     const phrases = [
-      "No",
-      "Are you sure?",
-      "What if I asked really nicely?",
-      "Pretty please",
-      "With a chocolate rice cake on top",
-      "What about a matcha frostie",
-      "PLEASE POOKIE",
-      "But :*(",
-      "I am going to die",
-      "Yep im dead",
-      "ok ur talking to nathan's ghost",
-      "please babe",
-      ":((((",
-      "PRETTY PLEASE",
-      "Estoy muerto",
-      "No :(",
+      "ไม่",
+      "บ่",
+      "ยกโทษให้โหน่ย",
+      "แง่ว",
+      "เค้าเลี้ยงหนม",
+      "เค้าเลี้ยงข้าว",
+      "เค้าทำงาบ้านให้",
+      "เค้าไม่ทำแน้ว",
+      "เค้ารักจจ",
+      ":(((",
+      "เค้าซื้อทองให้",
     ];
 
     return phrases[Math.min(noCount, phrases.length - 1)];
@@ -38,7 +33,7 @@ export default function Page() {
       {yesPressed ? (
         <>
           <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-          <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you pookie!! ;))</div>
+          <div className="my-4 text-4xl font-bold">เย้</div>
         </>
       ) : (
         <>
@@ -46,14 +41,14 @@ export default function Page() {
             className="h-[200px]"
             src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
           />
-          <h1 className="my-4 text-4xl">Will you be my Valentine?</h1>
+          <h1 className="my-4 text-4xl">เค้าขอโทษ ยกโทษให้เข้าได้ป่าวว</h1>
           <div className="flex items-center">
             <button
               className={`mr-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700`}
               style={{ fontSize: yesButtonSize }}
               onClick={() => setYesPressed(true)}
             >
-              Yes
+              ได้
             </button>
             <button
               onClick={handleNoClick}
